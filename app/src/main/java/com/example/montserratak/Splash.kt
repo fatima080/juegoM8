6954809d233723fc9bc22514f049d24c31ab7489
fatima080 <fatimazahraelbaz08@gmail.com>
2024-02-15 18:57:32 +0100

    2a pujada

--- app/src/main/java/com/example/montserratak/Splash.kt
+++ app/src/main/java/com/example/montserratak/Splash.kt
@@ -1,11 +1,36 @@
 package com.example.montserratak
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.widget.ImageView
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class Splash : AppCompatActivity() {
+    private val duracio: Long = 3000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        //amaguem la barra, pantalla a full
+        supportActionBar?.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        //val logo = findViewById<ImageView>(R.id.imageView)
+
+        //Glide.with(this).load(R.drawable.imagendiferencia).into(logo)
+
+        //cridem a la funció de canviar activitat
+        canviarActivity()
     }
-}
+
+    private fun canviarActivity() {
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, duracio)
+
+    }
+
+    }
